16115763034e330dc50b92cbdd5aac52597d7cd6
Julian Ganz <ganz@fzi.de>
2025-02-25 22:13:23 +0100

    tracer: add InstructionCache impl for fixed sized cache
    Currently, we compile and include `disassembler::InstructionCache` in
    `TraceState` only if we build with the `cache` feature. We decided to
    switch to another approach for compile time variability and abstract
    caches via a trait.
    
    We could impl the new `cache::InstructionCache` trait for the existing
    `disassembler::InstructionCache`. But we want to move it to a different
    location anyway. And the existing implementation does a lot of pointless
    copying. We threfore decided to supply a new implementation that will,
    in time, replace the old one.

--- src/tracer/cache.rs
+++ src/tracer/cache.rs
@@ -14,3 +14,43 @@ pub trait InstructionCache {
     /// Retrieve an [Instruction] by address
     fn get(&self, addr: u64) -> Option<Instruction>;
 }
+
+/// Simple [InstructionCache] of fixed size
+#[derive(Clone, Debug)]
+pub struct FixedSizedCache<const N: usize> {
+    data: [(u64, Instruction); N],
+    pos: usize,
+    count: usize,
+}
+
+impl<const N: usize> Default for FixedSizedCache<N> {
+    fn default() -> Self {
+        Self {
+            data: [Default::default(); N],
+            pos: 0,
+            count: 0,
+        }
+    }
+}
+
+impl<const N: usize> InstructionCache for FixedSizedCache<N> {
+    fn store(&mut self, addr: u64, insn: Instruction) {
+        self.data[self.pos] = (addr, insn);
+
+        self.pos += 1;
+        if self.pos > N {
+            self.pos = 0;
+        }
+
+        self.count = self.count.saturating_add(1);
+    }
+
+    fn get(&self, addr: u64) -> Option<Instruction> {
+        self.data
+            .iter()
+            .take(self.count)
+            .cloned()
+            .find(|(a, _)| *a == addr)
+            .map(|(_, i)| i)
+    }
+}
