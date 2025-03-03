13b4ee38ef9692d0b6c474a0d570edfdf804fd98
Julian Ganz <ganz@fzi.de>
2025-03-03 21:35:35 +0100

    decoder: impl core::error::Error for Error
    This makes `Error` usable as a proper error type. Which some users will
    probably want.

--- src/decoder.rs
+++ src/decoder.rs
@@ -49,6 +49,8 @@ pub enum Error {
     UnknownPrivilege(u8),
 }
 
+impl core::error::Error for Error {}
+
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
