3dadc1e29a2655ac1bc8d9c3da13a288c3e44eca
Julian Ganz <ganz@fzi.de>
2025-03-03 21:34:45 +0100

    decoder: impl Copy, Clone for Error
    There is no good reason why a (future) `Error` type should not impl at
    least `Clone`. And since it's a relatively simple smallish type, we can
    also make it `Copy` as well.

--- src/decoder.rs
+++ src/decoder.rs
@@ -29,7 +29,7 @@ impl Default for DecoderConfiguration {
 }
 
 /// A list of possible errors during decoding of a single packet.
-#[derive(Debug)]
+#[derive(Copy, Clone, Debug)]
 pub enum Error {
     /// [TraceType] does not indicate an instruction trace. The unknown trace type is returned.
     UnknownTraceType(u64),
