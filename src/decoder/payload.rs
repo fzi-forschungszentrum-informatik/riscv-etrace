94477a618af68d35adfa6ae4e48ed5e83709eb14
Julian Ganz <ganz@fzi.de>
2025-03-09 15:16:29 +0100

    decoder: add fn for reading implicit return info as an Option
    Currently, the `irreport` and `irdepth` fields defined by the
    specification for `AddressInfo` and `JumpTargetIndex` payloads are
    encapsulated in the `ImplicitReturn` that is only included and decoded
    with the `implicit_return` feature. We plan to avoid using features for
    compile time variance regarding protocol features. But rather than just
    removing the `#[cfg()]` directives, we decided to replace that struct
    entirely with a simple `Option` that captures the semantics of that
    struct.
    
    This change prepares this transition by introducing a fn that reads the
    `irreport` and `irdepth` fields and produces that `Option`. Rather than
    having the `Option` carry an `u64` like `irdepth` we decided on an
    `usize`, since the depth will specify the depth inside a buffer which we
    will need to allocate and index on the host rather than on the tracing
    target.

--- src/decoder/payload.rs
+++ src/decoder/payload.rs
@@ -26,6 +26,27 @@ fn read_branches(decoder: &mut Decoder) -> Result<(u8, u8), Error> {
     Ok((branches, len))
 }
 
+/// Read the `irreport` and `irdepth` fields
+///
+/// This fn reads the `irreport` and `irdepth` fields. The former is read
+/// differentially, and if the result is `true` this fn returns `irdepth`.
+/// Otherwise, `None` is returned.
+fn read_implicit_return(decoder: &mut Decoder) -> Result<Option<usize>, Error> {
+    let depth_len = decoder.proto_conf.return_stack_size_p
+        + decoder.proto_conf.call_counter_size_p
+        + (if decoder.proto_conf.return_stack_size_p > 0 {
+            1
+        } else {
+            0
+        });
+    // We intentionally read both the `irreport` and `irdepth` field
+    // unconditionally in order to keep the overall width read constant.
+    let report = decoder.read_differential_bit()?;
+    let depth = decoder.read_bits(depth_len)?;
+
+    Ok(report.then_some(depth))
+}
+
 /// The possible privilege levels with which the instruction was executed.
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub enum Privilege {
