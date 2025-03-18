c317b7052876575c0cdb7ef379748aab56bb91c1
Julian Ganz <ganz@fzi.de>
2025-03-18 20:21:42 +0100

    instruction: add error type expressing absence of Instructions
    We recently introduced a trait abstracting `Instruction` retrieval.
    Impls of that trait also need to supply an error type for when an
    instruction could not be retrieved. The most simple impls will likely
    just want to simply signal the absence of an `Instruction`.

--- src/instruction/binary.rs
+++ src/instruction/binary.rs
@@ -1,6 +1,8 @@
 // Copyright (C) 2025 FZI Forschungszentrum Informatik
 // SPDX-License-Identifier: Apache-2.0
 
+use core::fmt;
+
 use super::Instruction;
 
 /// A binary of some sort that contains [Instruction]s
@@ -17,5 +19,17 @@ impl<F: Fn(u64) -> Result<Instruction, E>, E> Binary for F {
 
     fn get_insn(&self, address: u64) -> Result<Instruction, Self::Error> {
         self(address)
+    }
+}
+
+/// An error type expressing simple absence of an [Instruction]
+#[derive(Copy, Clone, Debug)]
+pub struct NoInstruction;
+
+impl core::error::Error for NoInstruction {}
+
+impl fmt::Display for NoInstruction {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "No Instruction availible")
     }
 }
