dde3cbd86f5aa90ec5a6792016506f23d6b64fd5
Julian Ganz <ganz@fzi.de>
2025-03-28 19:20:06 +0100

    types: add trap::Info fn for convenient id of trap kinds
    For users' conveninence.

--- src/types/trap.rs
+++ src/types/trap.rs
@@ -8,6 +8,16 @@ pub struct Info {
     pub kind: Kind,
 }
 
+impl Info {
+    pub fn is_interrupt(&self) -> bool {
+        self.kind == Kind::Interrupt
+    }
+
+    pub fn is_exception(&self) -> bool {
+        self.kind == Kind::Exception
+    }
+}
+
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub enum Kind {
     Interrupt,
