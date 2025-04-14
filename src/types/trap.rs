bc742dd294a7a3e5dd1a5c96d4a7a0d400fbb5b5
Julian Ganz <ganz@fzi.de>
2025-04-14 19:54:03 +0200

    types: add missing documentation to trap::Info
    We were lacking documentation before, but the recent removal of the
    `kind` data member made documentation even more important to have.

--- src/types/trap.rs
+++ src/types/trap.rs
@@ -1,17 +1,25 @@
 // Copyright (C) 2025 FZI Forschungszentrum Informatik
 // SPDX-License-Identifier: Apache-2.0
 
+/// Information about a trap
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub struct Info {
+    /// Cause of the trap or interrupt
     pub ecause: u64,
+    /// Appropriate `utval`/`stval`/`vstval`/`mtval`
+    ///
+    /// This field also indicates whether the trap is an interrupt (`None`) or
+    /// exception (`Some`).
     pub tval: Option<u64>,
 }
 
 impl Info {
+    /// This info refers to an interrupt
     pub fn is_interrupt(&self) -> bool {
         self.tval.is_none()
     }
 
+    /// This info refers to an exception
     pub fn is_exception(&self) -> bool {
         self.tval.is_some()
     }
