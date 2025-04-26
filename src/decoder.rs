18f78a79e60ed98935efa6eb4f7573802eeaa0d7
Julian Ganz <ganz@fzi.de>
2025-04-26 14:36:38 +0200

    decoder: clean up documentation of Decoder

--- src/decoder.rs
+++ src/decoder.rs
@@ -54,10 +54,53 @@ impl fmt::Display for Error {
     }
 }
 
-/// A decoder for packets. The decoder is stateless in respect to a single packet parse.
-/// Multiple packets from different harts may be sequentially parsed by a single decoder
-/// instance as the decoder is stateless between [decode()](Decoder::decode_smi_packet())
-/// calls.
+/// A decoder for individual packets and/or [`Payload`][payload::Payload]s
+///
+/// Use this decoder to decode individual [`smi::Packet`]s.
+///
+/// A decoder is created and loaded with raw data via a [`Builder`]. From that
+/// data, the fn [`decode_smi_packet`][Self::decode_smi_packet] will decode one
+/// [`smi::Packet`]s containing a [`Payload`][payload::Payload]. Multiple
+/// packets from different harts may be sequentially decoded by a single decoder
+/// instance.
+///
+/// If a packet could not be decoded due to insufficient data, the decoder will
+/// report this by emitting an [`Error::InsufficientData`] error.
+/// Alternatively, the number of bytes left in the input can be queried via the
+/// fn [`bytes_left`][Self::bytes_left].
+///
+/// # Example
+///
+/// The follwing example demonstrates decoding of trace data in chunks with one
+/// decoder per input, including the recovery from attempting to decode an
+/// incomplete packet. We do not need to clone the [`Builder`] since it happens
+/// to be [`Copy`] in this case.
+///
+/// ```
+/// use riscv_etrace::decoder;
+///
+/// # let parameters = Default::default();
+/// # let trace_data = b"\x45\x73\x0a\x00";
+/// # let trace_data_next = b"\x45\x73\x0a\x00\x00\x20\x41\x01";
+/// let builder = decoder::Builder::new()
+///     .with_params(&parameters);
+/// let mut decoder = builder.build(trace_data);
+/// loop {
+///     match decoder.decode_smi_packet() {
+///         Ok(packet) => eprintln!("{packet:?}", ),
+///         Err(decoder::Error::InsufficientData(_)) => break,
+///         Err(e) => panic!("{e:?}"),
+///     }
+/// }
+/// let mut decoder = builder.build(trace_data_next);
+/// loop {
+///     match decoder.decode_smi_packet() {
+///         Ok(packet) => eprintln!("{packet:?}", ),
+///         Err(decoder::Error::InsufficientData(_)) => break,
+///         Err(e) => panic!("{e:?}"),
+///     }
+/// }
+/// ```
 #[derive(Clone)]
 pub struct Decoder<'d, U> {
     data: &'d [u8],
@@ -125,8 +168,8 @@ impl<U> Decoder<'_, U> {
 
     /// Read a single differential bit
     ///
-    /// The bit's value is considered to be `true` if it differs from the
-    /// previous bit and `false` if it doesn't.
+    /// The bit's value is considered to be [`true`] if it differs from the
+    /// previous bit and [`false`] if it doesn't.
     fn read_differential_bit(&mut self) -> Result<bool, Error> {
         let reference_pos = self
             .bit_pos
