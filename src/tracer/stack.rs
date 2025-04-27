d53fd85edc16a120e838e3309d2d22285b9770a9
Julian Ganz <ganz@fzi.de>
2025-04-27 13:07:47 +0200

    tracer: clean up documentation of stack::ReturnStack

--- src/tracer/stack.rs
+++ src/tracer/stack.rs
@@ -8,15 +8,15 @@
 pub trait ReturnStack: Sized {
     /// Create a new return stack with the given maximum depth
     ///
-    /// Returns `None` if `max_depth` is greater than the value supported by the
-    /// implementaiton, or if the stack could not be created due to some other
-    /// reason.
+    /// Returns [`None`] if `max_depth` is greater than the value supported by
+    /// the implementaiton, or if the stack could not be created due to some
+    /// other reason.
     fn new(max_depth: usize) -> Option<Self>;
 
     /// Push a new return address on the stack
     ///
     /// If the maximal depth is reached, the bottom address will be evicted from
-    /// the stack and thus no longer be obtainable via a [Self::pop].
+    /// the stack and thus no longer be obtainable via a [`pop`][Self::pop].
     fn push(&mut self, addr: u64);
 
     /// Retrieve and remove the topmost return address
