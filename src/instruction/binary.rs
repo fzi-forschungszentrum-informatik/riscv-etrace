6b187ce7de02026902f786189265d95c0fcee3b9
Julian Ganz <ganz@fzi.de>
2025-07-15 11:11:55 +0200

    instruction: add slice-like compond binary::Binary
    We may need to handle multiple `binary::Binary`s of the same type in the
    future, e.g. an ELF each for firmware and application. And in some
    cases, the number is not fixed.
    
    In such a situation, it seems natural to iterate over the ELFs and yield
    the first result which is not a "miss", using the recently introduced
    `MaybeMiss`. This change introduces an utility for doing so,
    encapsulating a slice and doing some basic caching wrt. the `Binary`
    selection.

--- src/instruction/binary.rs
+++ src/instruction/binary.rs
@@ -5,6 +5,7 @@
 //! This module defines the [`Binary`] trait for programs that may be traced as
 //! well as a number of types that may serve as a [`Binary`].
 
+use core::borrow::BorrowMut;
 use core::fmt;
 
 use super::Instruction;
@@ -80,6 +81,79 @@ where
         self.as_mut()
             .map(|b| b.get_insn(address))
             .unwrap_or_else(|| MaybeMiss::miss(address))
+    }
+}
+
+/// Set of [`Binary`] acting as a single [`Binary`]
+#[derive(Copy, Clone, Default, Debug)]
+pub struct Multi<C, B>
+where
+    C: BorrowMut<[B]>,
+    B: Binary,
+    B::Error: MaybeMiss,
+{
+    bins: C,
+    last: usize,
+    phantom: core::marker::PhantomData<B>,
+}
+
+impl<C, B> Multi<C, B>
+where
+    C: BorrowMut<[B]>,
+    B: Binary,
+    B::Error: MaybeMiss,
+{
+    /// Create a new [`Binary`] combining all `bins`
+    pub fn new(bins: C) -> Self {
+        Self {
+            bins,
+            last: 0,
+            phantom: Default::default(),
+        }
+    }
+}
+
+impl<C, B> From<C> for Multi<C, B>
+where
+    C: BorrowMut<[B]>,
+    B: Binary,
+    B::Error: MaybeMiss,
+{
+    fn from(bins: C) -> Self {
+        Self::new(bins)
+    }
+}
+
+impl<C, B> Binary for Multi<C, B>
+where
+    C: BorrowMut<[B]>,
+    B: Binary,
+    B::Error: MaybeMiss,
+{
+    type Error = B::Error;
+
+    fn get_insn(&mut self, address: u64) -> Result<Instruction, Self::Error> {
+        let bins = self.bins.borrow_mut();
+        let res = bins
+            .get_mut(self.last)
+            .map(|b| b.get_insn(address))
+            .filter(|r| !r.is_miss());
+        if let Some(res) = res {
+            return res;
+        }
+
+        let res = bins
+            .iter_mut()
+            .enumerate()
+            .filter(|(n, _)| *n != self.last)
+            .map(|(n, b)| (n, b.get_insn(address)))
+            .find(|(_, r)| !r.is_miss());
+        if let Some((current, res)) = res {
+            self.last = current;
+            res
+        } else {
+            MaybeMiss::miss(address)
+        }
     }
 }
 
