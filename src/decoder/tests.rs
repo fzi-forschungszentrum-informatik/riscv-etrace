a7033bb1fe6409ad1964a66af52169accfa1c099
Julian Ganz <ganz@fzi.de>
2025-08-03 21:55:49 +0200

    decoder: rewrite decoder tests for payloads using bitstream_test!

--- src/decoder/tests.rs
+++ src/decoder/tests.rs
@@ -2,6 +2,8 @@
 // SPDX-License-Identifier: Apache-2.0
 use super::*;
 
+use crate::types::{self, branch};
+
 use payload::AddressInfo;
 
 macro_rules! bitstream_test {
@@ -96,144 +98,61 @@ bitstream_test!(fmt_3, b"\x02", format::Format::Addr, f0s_width_p: 1);
 bitstream_test!(fmt_4, b"\x07", format::Format::Sync(format::Sync::Trap), f0s_width_p: 1);
 
 // `payload` related tests
+bitstream_test!(
+    extension_jti_1,
+    b"\x00\x7f\x05",
+    payload::JumpTargetIndex { index: 768, branch_map: branch::Map::new(31, 10), irdepth: None },
+    cache_size_p: 10
+);
+bitstream_test!(
+    extension_jti_2,
+    b"\xff\x03",
+    payload::JumpTargetIndex { index: 1023, branch_map: Default::default(), irdepth: None },
+    cache_size_p: 10
+);
+bitstream_test!(
+    branch,
+    b"\x47\x0b",
+    payload::Branch {
+        branch_map: branch::Map::new(7, 0b1011_010),
+        address: Some(AddressInfo { address: 0, notify: false, updiscon: false, irdepth: None }),
+    },
+    cache_size_p: 10
+);
+bitstream_test!(
+    branch_with_zero_branches,
+    b"\x00\x04",
+    payload::Branch {
+        branch_map: branch::Map::new(31, 32),
+        address: None
+    }
+);
+bitstream_test!(
+    address_absolute,
+    b"\x01\x00\x00\x00\x00\x00\x00\xc0",
+    payload::AddressInfo { address: 4, notify: true, updiscon: false, irdepth: None },
+    iaddress_width_p: 64.try_into().unwrap(),
+    iaddress_lsb_p: 2.try_into().unwrap()
 
-#[test]
-fn extension_jti_1() {
-    let protocol_config = Default::default();
+);
+bitstream_test!(
+    address_differential,
+    b"\x01\x00\x00\x00\x00\x00\x00\x80",
+    payload::AddressInfo { address: 4, notify: false, updiscon: true, irdepth: None },
+    iaddress_width_p: 64.try_into().unwrap(),
+    iaddress_lsb_p: 2.try_into().unwrap()
 
-    let mut buffer = [0; DEFAULT_PACKET_BUFFER_LEN];
-    buffer[0] = 0b00000000;
-    buffer[1] = 0b0_11111_11;
-    buffer[2] = 0b00000_101;
-
-    let mut decoder = Builder::new()
-        .with_params(&config::Parameters {
-            cache_size_p: 10,
-            ..protocol_config
-        })
-        .build(&buffer);
-
-    let jti_long = payload::JumpTargetIndex::decode(&mut decoder).unwrap();
-    assert_eq!(jti_long.index, 768);
-    assert_eq!(jti_long.branch_map.count(), 31);
-    assert_eq!(jti_long.branch_map.raw_map(), 10);
-}
-
-#[test]
-fn extension_jti_2() {
-    let protocol_config = Default::default();
-
-    let mut buffer = [0; DEFAULT_PACKET_BUFFER_LEN];
-    buffer[0] = 0b11111111;
-    buffer[1] = 0b00000011;
-    let mut decoder = Builder::new()
-        .with_params(&config::Parameters {
-            cache_size_p: 10,
-            ..protocol_config
-        })
-        .build(&buffer);
-
-    let jti_short = payload::JumpTargetIndex::decode(&mut decoder).unwrap();
-    assert_eq!(jti_short.index, 1023);
-    assert_eq!(jti_short.branch_map, Default::default());
-}
-
-#[test]
-fn branch() {
-    let mut buffer = [0; DEFAULT_PACKET_BUFFER_LEN];
-    buffer[0] = 0b010_00111;
-    buffer[1] = 0b0000_1011;
-    let mut decoder = Builder::new().build(&buffer);
-    let branch = payload::Branch::decode(&mut decoder).unwrap();
-    assert_eq!(branch.branch_map.count(), 7);
-    assert_eq!(branch.branch_map.raw_map(), 0b1011_010);
-    assert_eq!(
-        branch.address,
-        Some(AddressInfo {
-            address: 0,
-            notify: false,
-            updiscon: false,
-            irdepth: None,
-        })
-    );
-}
-
-#[test]
-fn branch_with_zero_branches() {
-    let mut buffer = [0; DEFAULT_PACKET_BUFFER_LEN];
-    buffer[0] = 0b000_00000;
-    buffer[1] = 0b100;
-    let mut decoder = Builder::new().build(&buffer);
-    let branch_no_addr = payload::Branch::decode(&mut decoder).unwrap();
-    assert_eq!(branch_no_addr.branch_map.count(), 31);
-    assert_eq!(branch_no_addr.branch_map.raw_map(), 32);
-    assert_eq!(branch_no_addr.address, None);
-}
-
-#[test]
-fn address_absolute() {
-    let protocol_config = Default::default();
-
-    let mut buffer = [0; DEFAULT_PACKET_BUFFER_LEN];
-    buffer[0] = 0b0000_0001;
-    buffer[7] = 0b11_000000;
-    let mut decoder = Builder::new()
-        .with_params(&config::Parameters {
-            // Changed address width and lsb, so that the entire
-            // packet aligns with 64 bit
-            iaddress_width_p: 64.try_into().unwrap(),
-            iaddress_lsb_p: 2.try_into().unwrap(),
-            ..protocol_config
-        })
-        .build(&buffer);
-
-    let addr = AddressInfo::decode(&mut decoder).unwrap();
-    assert_eq!(addr.address, 4);
-    assert!(addr.notify);
-    assert!(!addr.updiscon);
-}
-
-#[test]
-fn address_differential() {
-    let protocol_config = Default::default();
-
-    let mut buffer = [0; DEFAULT_PACKET_BUFFER_LEN];
-    buffer[0] = 0b0000_0001;
-    buffer[7] = 0b10_000000;
-    let mut decoder = Builder::new()
-        .with_params(&config::Parameters {
-            // Changed address width and lsb, so that the entire
-            // packet aligns with 64 bit
-            iaddress_width_p: 64.try_into().unwrap(),
-            iaddress_lsb_p: 2.try_into().unwrap(),
-            ..protocol_config
-        })
-        .build(&buffer);
-
-    let diff_addr = AddressInfo::decode(&mut decoder).unwrap();
-    assert_eq!(diff_addr.address, 4);
-    assert!(!diff_addr.notify);
-    assert!(diff_addr.updiscon);
-}
-
-#[test]
-fn synchronization_start() {
-    use crate::types::Privilege;
-
-    let protocol_config = Default::default();
-
-    let buffer = [255; DEFAULT_PACKET_BUFFER_LEN];
-    let mut decoder = Builder::new()
-        .with_params(&config::Parameters {
-            iaddress_width_p: 64.try_into().unwrap(),
-            iaddress_lsb_p: 1.try_into().unwrap(),
-            ..protocol_config
-        })
-        .build(&buffer);
-    let sync_start = sync::Start::decode(&mut decoder).unwrap();
-    assert!(sync_start.branch);
-    assert_eq!(sync_start.ctx.privilege, Privilege::Machine);
-    assert_eq!(sync_start.address, 0xffff_ffff_ffff_fffe);
-}
+);
+bitstream_test!(
+    synchronization_start,
+    b"\xff",
+    sync::Start {
+        branch: true,
+        ctx: sync::Context { privilege: types::Privilege::Machine, time: None, context: None },
+        address: 0xffff_ffff_ffff_fffe,
+    },
+    iaddress_width_p: 64.try_into().unwrap(),
+    iaddress_lsb_p: 1.try_into().unwrap()
+);
 
 const DEFAULT_PACKET_BUFFER_LEN: usize = 32;
