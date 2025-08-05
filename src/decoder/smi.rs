d1f9817e7ad01802f481ef66674eff29e947d72d
Julian Ganz <ganz@fzi.de>
2025-08-05 15:39:28 +0200

    decoder: remove smi::Header
    Previously, `smi::Packet` would contain an `smi::Header`. However, we
    replaces the header with individual values if they were relevant for
    tracer operations.

--- src/decoder/smi.rs
+++ src/decoder/smi.rs
@@ -46,46 +46,6 @@ impl<U: unit::Unit> Decode<U> for Packet<U::IOptions, U::DOptions> {
     }
 }
 
-/// Siemens Messaging Infrastructure Packet header
-///
-/// This type represents a decoded header of a Siemens Messaging Infrastructure
-/// (SMI) Packet as described in Chapter 7. Instruction Trace Encoder Output
-/// Packets of the specification.
-#[derive(Debug, Eq, PartialEq)]
-pub struct Header {
-    /// [`Payload`][payload::Payload] length in bytes.
-    pub payload_len: usize,
-    /// Destination flow indicator, which we use for the trace type
-    pub trace_type: TraceType,
-    /// Partial time stamp
-    pub time_tag: Option<u16>,
-    /// Index of the hart this packet is originating from
-    ///
-    /// The index specifies the address of the hart's trace unit within the
-    /// messaging infrastructure. It may not be identical to the value of the
-    /// `mhartid` CSR for that hart.
-    pub hart_index: usize,
-}
-
-impl<U> Decode<U> for Header {
-    fn decode(decoder: &mut Decoder<U>) -> Result<Self, Error> {
-        let payload_len = decoder.read_bits(5)?;
-        let trace_type = TraceType::decode(decoder)?;
-        let time_tag = decoder
-            .read_bit()?
-            .then(|| decoder.read_bits(16))
-            .transpose()?;
-        let hart_index = decoder.read_bits(decoder.hart_index_width)?;
-
-        Ok(Header {
-            payload_len,
-            trace_type,
-            time_tag,
-            hart_index,
-        })
-    }
-}
-
 /// Destination flow indicator, which we use for the trace type
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub enum TraceType {
