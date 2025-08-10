663d44c3a3a62cb86bd42db6c4dd061e3998476a
Julian Ganz <ganz@fzi.de>
2025-08-10 14:40:44 +0200

    binary: add a Binary wrapping an FnMut
    We currently have a blanket impl of `Binary` for `FnMut`s. This impl can
    be useful, but obstructs other very useful impls, e.g. for "reference"
    types. We therefore decided to remove it. But we do want to preserve the
    option to plug in a `Fn` as a `Binary`. This change introduces a small
    wrapper for that purpose.

--- src/binary.rs
+++ src/binary.rs
@@ -182,6 +182,33 @@ impl<B: Binary> Binary for Offset<B> {
     }
 }
 
+/// [`Binary`] adapter for an [`FnMut`]
+///
+/// This forwards calls to [`Binary::get_insn`] to the wrapped [`FnMut`].
+#[derive(Copy, Clone, Default, Debug)]
+pub struct Func<F: FnMut(u64) -> Result<Instruction, E>, E> {
+    func: F,
+    phantom: core::marker::PhantomData<E>,
+}
+
+impl<F: FnMut(u64) -> Result<Instruction, E>, E> Func<F, E> {
+    /// Create a new [`Binary`] from an [`FnMut`]
+    fn new(func: F) -> Self {
+        Self {
+            func,
+            phantom: Default::default(),
+        }
+    }
+}
+
+impl<F: FnMut(u64) -> Result<Instruction, E>, E> Binary for Func<F, E> {
+    type Error = E;
+
+    fn get_insn(&mut self, address: u64) -> Result<Instruction, Self::Error> {
+        (self.func)(address)
+    }
+}
+
 /// A [`Binary`] that does not contain any [`Instruction`]s
 #[derive(Copy, Clone, Default, Debug)]
 pub struct Empty;
