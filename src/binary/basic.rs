f190319f8fa2a8d247341a281d7545be84f72454
Julian Ganz <ganz@fzi.de>
2025-08-11 22:03:58 +0200

    binary: add Binary for simple maps
    Currently, `&[(u64, Instrction)]` impls `Binary`. This did serve its
    original purpose, but it's also somewhat restrictive as it does not
    enable using a `Vec` directly. We cannot and should not introduce a
    blanket impl for all types implementing `AsRef` since that would
    conflict with other impls.
    
    Just like for `FnMut`, we thus decided to introduce a `Binary` adapter.
    And sicne we control how it's created, we can enforce the slice being
    sorted by address.

--- src/binary/basic.rs
+++ src/binary/basic.rs
@@ -39,6 +39,67 @@ pub fn from_fn<F: FnMut(u64) -> Result<Instruction, E>, E>(func: F) -> Func<F, E
     Func::new(func)
 }
 
+/// [`Binary`] defined by a set of addresses-[`Instruction`] pairs
+///
+/// This [`Binary`] is backed by a slice of addresses-[`Instruction`] pairs
+/// specifying the presence of an [`Instruction`] at the specified address. The
+/// [`Binary`] is meant for small, fixed code sequences such as bootroms.
+#[derive(Copy, Clone, Default, Debug)]
+pub struct SimpleMap<T: AsRef<[(u64, Instruction)]>> {
+    inner: T,
+}
+
+impl<T: AsRef<[(u64, Instruction)]>> SimpleMap<T> {
+    /// Create a new [`Binary`], potentially from a different type of container
+    ///
+    /// Prepares the slice held by the given container, then converts this to
+    /// the target type and returns the [`Binary`] contructed from that. This
+    /// allows creating a [`Binary`] operating on an [`Arc`][alloc::sync::Arc]
+    /// from containers that allow mutation of the slice such as
+    /// [`Box`][alloc::boxed::Box].
+    pub fn new<I>(mut inner: I) -> Self
+    where
+        T: From<I>,
+        I: AsMut<[(u64, Instruction)]>,
+    {
+        inner.as_mut().sort_unstable_by_key(|(a, _)| *a);
+        Self {
+            inner: inner.into(),
+        }
+    }
+
+    /// Create a [`Binary`] from a container holding a sorted slice
+    ///
+    /// Returns [`None`] if the slice is not sorted by address.
+    pub fn from_sorted(inner: T) -> Option<Self> {
+        inner
+            .as_ref()
+            .is_sorted_by_key(|(a, _)| *a)
+            .then_some(Self { inner })
+    }
+}
+
+impl<T, I> From<I> for SimpleMap<T>
+where
+    T: AsRef<[(u64, Instruction)]> + From<I>,
+    I: AsMut<[(u64, Instruction)]>,
+{
+    fn from(inner: I) -> Self {
+        Self::new(inner)
+    }
+}
+
+impl<T: AsRef<[(u64, Instruction)]>> Binary for SimpleMap<T> {
+    type Error = error::NoInstruction;
+
+    fn get_insn(&mut self, address: u64) -> Result<Instruction, Self::Error> {
+        let map = self.inner.as_ref();
+        map.binary_search_by_key(&address, |(a, _)| *a)
+            .map(|i| map[i].1)
+            .map_err(|_| error::NoInstruction)
+    }
+}
+
 /// A [`Binary`] that does not contain any [`Instruction`]s
 #[derive(Copy, Clone, Default, Debug)]
 pub struct Empty;
