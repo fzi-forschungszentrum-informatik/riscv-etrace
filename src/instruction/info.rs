51240a8eee39b5db9959016c435e87c590009ebb
Julian Ganz <ganz@fzi.de>
2025-10-02 11:47:40 +0200

    instruction: impl info::Info for riscv_isa::Instruction
    Recent changes allow us to add support for off-the-shelf disassemblers.
    We decided to add support for `riscv-isa`. This change enables the use
    of use of `riscv_isa::Instruction`s as an `info::Info` when tracing.

--- src/instruction/info.rs
+++ src/instruction/info.rs
@@ -165,6 +165,64 @@ impl<T: Info> Info for Option<T> {
     }
 }
 
+#[cfg(feature = "riscv-isa")]
+impl Info for riscv_isa::Instruction {
+    type Register = u32;
+
+    fn branch_target(&self) -> Option<i16> {
+        match self {
+            Self::BEQ { offset, .. } => Some(*offset as i16),
+            Self::BNE { offset, .. } => Some(*offset as i16),
+            Self::BLT { offset, .. } => Some(*offset as i16),
+            Self::BGE { offset, .. } => Some(*offset as i16),
+            Self::BLTU { offset, .. } => Some(*offset as i16),
+            Self::BGEU { offset, .. } => Some(*offset as i16),
+            _ => None,
+        }
+    }
+
+    fn inferable_jump_target(&self) -> Option<i32> {
+        match self {
+            Self::JALR { rs1: 0, offset, .. } => Some(*offset),
+            Self::JAL { offset, .. } => Some(*offset),
+            _ => None,
+        }
+    }
+
+    fn uninferable_jump_target(&self) -> Option<(Self::Register, i16)> {
+        match self {
+            Self::JALR { rs1, offset, .. } => Some((*rs1, *offset as i16)),
+            _ => None,
+        }
+        .filter(|(r, _)| *r != 0)
+    }
+
+    fn upper_immediate(&self, pc: u64) -> Option<(Self::Register, u64)> {
+        match self {
+            Self::LUI { rd, imm } => Some((*rd, 0, *imm as i32)),
+            Self::AUIPC { rd, imm } => Some((*rd, pc, *imm as i32)),
+            _ => None,
+        }
+        .map(|(r, b, o)| (r, b.wrapping_add_signed((o << 12).into())))
+    }
+
+    fn is_return_from_trap(&self) -> bool {
+        matches!(self, Self::SRET | Self::MRET)
+    }
+
+    fn is_ecall_or_ebreak(&self) -> bool {
+        matches!(self, Self::ECALL | Self::EBREAK)
+    }
+
+    fn is_call(&self) -> bool {
+        matches!(self, Self::JALR { rd: 1, .. } | Self::JAL { rd: 1, .. })
+    }
+
+    fn is_return(&self) -> bool {
+        matches!(self, Self::JALR { rd: 0, rs1: 1, .. })
+    }
+}
+
 /// Decode for instruction [`Info`]
 pub trait Decode<I: Info> {
     /// Decode a 16bit ("compressed") instruction [`Info`]
