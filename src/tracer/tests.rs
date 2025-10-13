7cef1a5929de7e5b7c94a90cb7eb729d2057dfb1
Julian Ganz <ganz@fzi.de>
2025-10-13 11:53:39 +0200

    tracer: add tests for behaviour on exception after a branch

--- src/tracer/tests.rs
+++ src/tracer/tests.rs
@@ -273,6 +273,70 @@ trace_test!(
 );
 
 trace_test!(
+    exception_after_branch_taken,
+    test_bin_1(),
+    sync::Start {
+        branch: false,
+        ctx: Default::default(),
+        address: 0x8000001c,
+    } => {
+        (0x8000001c, Context::default()),
+        (0x8000001c, Kind::new_bltu(11, 12, -8))
+    }
+    sync::Trap {
+        branch: false,
+        ctx: Default::default(),
+        thaddr: true,
+        address: 0x80000030,
+        info: trap::Info { ecause: 2, tval: Some(0) },
+    } => {
+        (0x80000014, trap::Info { ecause: 2, tval: Some(0) }),
+        (0x80000030, Context::default()),
+        (0x80000030, Kind::wfi)
+    }
+    payload::AddressInfo {
+        address: 4,
+        notify: false,
+        updiscon: false,
+        irdepth: None,
+    } => {
+        (0x80000034, Kind::new_c_j(0, -4))
+    }
+);
+
+trace_test!(
+    exception_after_branch_not_taken,
+    test_bin_1(),
+    sync::Start {
+        branch: true,
+        ctx: Default::default(),
+        address: 0x8000001c,
+    } => {
+        (0x8000001c, Context::default()),
+        (0x8000001c, Kind::new_bltu(11, 12, -8))
+    }
+    sync::Trap {
+        branch: true,
+        ctx: Default::default(),
+        thaddr: true,
+        address: 0x80000030,
+        info: trap::Info { ecause: 2, tval: Some(0) },
+    } => {
+        (0x80000020, trap::Info { ecause: 2, tval: Some(0) }),
+        (0x80000030, Context::default()),
+        (0x80000030, Kind::wfi)
+    }
+    payload::AddressInfo {
+        address: 4,
+        notify: false,
+        updiscon: false,
+        irdepth: None,
+    } => {
+        (0x80000034, Kind::new_c_j(0, -4))
+    }
+);
+
+trace_test!(
     exception_after_updiscon,
     test_bin_1(),
     start_packet(0x80000024) => {
