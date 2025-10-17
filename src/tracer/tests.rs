aae6d517c0c67e05a562ad4ae6a3c382332e1ee6
Julian Ganz <ganz@fzi.de>
2025-10-17 16:36:34 +0200

    tracer: add test for sequentially inferable jumps

--- src/tracer/tests.rs
+++ src/tracer/tests.rs
@@ -813,6 +813,61 @@ trace_test!(
     }
 );
 
+trace_test!(
+    fn_calls_sijump,
+    test_bin_fncalls(),
+    @params {
+        sijump_p: true
+    }
+    start_packet(0x80000000) => {
+        (0x80000000, Context::default()),
+        (0x80000000, Kind::new_auipc(13, 0x0))
+    }
+    payload::AddressInfo {
+        address: 0x0e,
+        notify: false,
+        updiscon: false,
+        irdepth: None,
+    } => {
+        (0x80000004, UNCOMPRESSED),
+        (0x80000008, UNCOMPRESSED),
+        (0x8000000c, Kind::new_c_jal(1, 0x14)),
+        (0x80000020, COMPRESSED),
+        (0x80000022, Kind::new_c_jr(1)),
+        (0x8000000e, Kind::new_auipc(13, 0))
+    }
+    payload::AddressInfo {
+        address: 0x16-0x0e,
+        notify: false,
+        updiscon: false,
+        irdepth: None,
+    } => {
+        (0x80000012, Kind::new_jalr(1, 13, 0x12)),
+        (0x80000020, COMPRESSED),
+        (0x80000022, Kind::new_c_jr(1)),
+        (0x80000016, Kind::new_lui(13,0x80000000u32 as i32))
+    }
+    payload::AddressInfo {
+        address: 0x1e-0x16,
+        notify: false,
+        updiscon: false,
+        irdepth: None,
+    } => {
+        (0x8000001a, Kind::new_jalr(1, 13, 0x20)),
+        (0x80000020, COMPRESSED),
+        (0x80000022, Kind::new_c_jr(1)),
+        (0x8000001e, Kind::new_c_j(0, 0x6))
+    }
+    payload::AddressInfo {
+        address: 0x24-0x1e,
+        notify: false,
+        updiscon: false,
+        irdepth: None,
+    } => {
+        (0x80000024, Kind::wfi)
+    }
+);
+
 fn start_packet(address: u64) -> payload::InstructionTrace {
     sync::Start {
         branch: true,
