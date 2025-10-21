f3c53fbe3d03ca265ff5806d7c1c99120b43c3e9
Julian Ganz <ganz@fzi.de>
2025-10-21 18:49:28 +0200

    tracer: move tests for section 7.6.2 scenarios to dedicated submodule
    We decided to split parts of `tests` into smaller submodules.

--- src/tracer/tests.rs
+++ src/tracer/tests.rs
@@ -5,6 +5,7 @@
 mod gen;
 
 mod chapter12;
+mod section762;
 mod traps;
 
 use super::*;
@@ -141,155 +142,6 @@ trace_test!(
         (0x8000000c, Kind::new_auipc(1, 0x0)),
         (0x80000010, UNCOMPRESSED),
         (0x80000014, COMPRESSED)
-    }
-);
-
-// Case 1 described in section 7.6.2 of the spec (version 2.0.3)
-trace_test!(
-    strange_loop_regular,
-    test_bin_jr_loop(),
-    start_packet(0x8000000c) => {
-        (0x8000000c, Context::default()),
-        (0x8000000c, Kind::new_auipc(5, 0x0))
-    }
-    payload::AddressInfo {
-        address: 18,
-        notify: false,
-        updiscon: false,
-        irdepth: None,
-    } => {
-        (0x80000010, UNCOMPRESSED),
-        (0x80000014, Kind::new_auipc(11, 0)),
-        (0x80000018, UNCOMPRESSED),
-        (0x8000001c, COMPRESSED),
-        (0x8000001e, UNCOMPRESSED)
-    }
-    payload::AddressInfo {
-        address: 4,
-        notify: false,
-        updiscon: false,
-        irdepth: None,
-    } => {
-        (0x80000022, COMPRESSED),
-        (0x80000024, UNCOMPRESSED),
-        (0x80000028, UNCOMPRESSED),
-        (0x8000002c, UNCOMPRESSED),
-        (0x80000030, UNCOMPRESSED),
-        (0x80000034, COMPRESSED),
-        (0x80000036, Kind::new_c_jr(5)),
-        (0x8000001e, UNCOMPRESSED),
-        (0x80000022, COMPRESSED)
-    }
-);
-
-// Case 2 described in section 7.6.2 of the spec (version 2.0.3)
-trace_test!(
-    strange_loop_updiscon,
-    test_bin_jr_loop(),
-    start_packet(0x8000000c) => {
-        (0x8000000c, Context::default()),
-        (0x8000000c, Kind::new_auipc(5, 0x0))
-    }
-    payload::AddressInfo {
-        address: 18,
-        notify: false,
-        updiscon: true,
-        irdepth: None,
-    } => {
-        (0x80000010, UNCOMPRESSED),
-        (0x80000014, Kind::new_auipc(11, 0)),
-        (0x80000018, UNCOMPRESSED),
-        (0x8000001c, COMPRESSED),
-        (0x8000001e, UNCOMPRESSED),
-        (0x80000022, COMPRESSED),
-        (0x80000024, UNCOMPRESSED),
-        (0x80000028, UNCOMPRESSED),
-        (0x8000002c, UNCOMPRESSED),
-        (0x80000030, UNCOMPRESSED),
-        (0x80000034, COMPRESSED),
-        (0x80000036, Kind::new_c_jr(5)),
-        (0x8000001e, UNCOMPRESSED)
-    }
-    sync::Trap {
-        branch: true,
-        ctx: Default::default(),
-        thaddr: true,
-        address: 0x80000038,
-        info: trap::Info { ecause: 2, tval: Some(0) },
-    } => {
-        (0x80000022, trap::Info { ecause: 2, tval: Some(0) }),
-        (0x80000038, Context::default()),
-        (0x80000038, Kind::wfi)
-    }
-);
-
-// Case 3 described in section 7.6.2 of the spec (version 2.0.3)
-trace_test!(
-    strange_loop_exception,
-    test_bin_jr_loop(),
-    start_packet(0x8000000c) => {
-        (0x8000000c, Context::default()),
-        (0x8000000c, Kind::new_auipc(5, 0x0))
-    }
-    payload::AddressInfo {
-        address: 18,
-        notify: false,
-        updiscon: false,
-        irdepth: None,
-    } => {
-        (0x80000010, UNCOMPRESSED),
-        (0x80000014, Kind::new_auipc(11, 0)),
-        (0x80000018, UNCOMPRESSED),
-        (0x8000001c, COMPRESSED),
-        (0x8000001e, UNCOMPRESSED)
-    }
-    sync::Trap {
-        branch: true,
-        ctx: Default::default(),
-        thaddr: true,
-        address: 0x80000038,
-        info: trap::Info { ecause: 2, tval: Some(0) },
-    } => {
-        (0x80000022, trap::Info { ecause: 2, tval: Some(0) }),
-        (0x80000038, Context::default()),
-        (0x80000038, Kind::wfi)
-    }
-);
-
-// Case 4 described in section 7.6.2 of the spec (version 2.0.3)
-trace_test!(
-    strange_loop_notify,
-    test_bin_jr_loop(),
-    start_packet(0x8000000c) => {
-        (0x8000000c, Context::default()),
-        (0x8000000c, Kind::new_auipc(5, 0x0))
-    }
-    payload::AddressInfo {
-        address: 18,
-        notify: true,
-        updiscon: false,
-        irdepth: None,
-    } => {
-        (0x80000010, UNCOMPRESSED),
-        (0x80000014, Kind::new_auipc(11, 0)),
-        (0x80000018, UNCOMPRESSED),
-        (0x8000001c, COMPRESSED),
-        (0x8000001e, UNCOMPRESSED)
-    }
-    payload::AddressInfo {
-        address: 0,
-        notify: false,
-        updiscon: false,
-        irdepth: None,
-    } => {
-        (0x80000022, COMPRESSED),
-        (0x80000024, UNCOMPRESSED),
-        (0x80000028, UNCOMPRESSED),
-        (0x8000002c, UNCOMPRESSED),
-        (0x80000030, UNCOMPRESSED),
-        (0x80000034, COMPRESSED),
-        (0x80000036, Kind::new_c_jr(5)),
-        (0x8000001e, UNCOMPRESSED)
     }
 );
 
@@ -445,28 +297,6 @@ fn test_bin_1() -> [(u64, instruction::Instruction); 17] {
         // _die
         (0x80000030, Kind::wfi.into()),
         (0x80000034, Kind::new_c_j(0, -4).into()),
-    ]
-}
-
-fn test_bin_jr_loop() -> [(u64, instruction::Instruction); 15] {
-    [
-        (0x8000000c, Kind::new_auipc(5, 0).into()),
-        (0x80000010, UNCOMPRESSED),
-        (0x80000014, Kind::new_auipc(11, 0).into()),
-        (0x80000018, UNCOMPRESSED),
-        (0x8000001c, COMPRESSED),
-        // _loop_entry
-        (0x8000001e, UNCOMPRESSED),
-        (0x80000022, COMPRESSED),
-        (0x80000024, UNCOMPRESSED),
-        (0x80000028, UNCOMPRESSED),
-        (0x8000002c, UNCOMPRESSED),
-        (0x80000030, UNCOMPRESSED),
-        (0x80000034, COMPRESSED),
-        (0x80000036, Kind::new_c_jr(5).into()),
-        // _die
-        (0x80000038, Kind::wfi.into()),
-        (0x8000003c, Kind::new_c_j(0, -4).into()),
     ]
 }
 
