1084680ec93d71ed61a2d5da75860dfc48936258
Julian Ganz <ganz@fzi.de>
2025-10-31 16:38:54 +0100

    tracer: add basic "jumps" test for implicit return behaviour
    The implicit return option is known to be broken in the reference flow's
    encoder. We therefore cannot verify the many test-cases we would want to
    write. This test-case is, however, simple enough that both the reference
    flow does the right thing, i.e. we can reason about the trace it
    produces is correct.

--- src/tracer/tests/jumps.rs
+++ src/tracer/tests/jumps.rs
@@ -125,6 +125,55 @@ trace_test!(
     }
 );
 
+trace_test!(
+    ir_return_stack,
+    test_bin_fncalls(),
+    @implicit_return true
+    @params {
+        return_stack_size_p: 2
+    }
+    start_packet(0x80000000) => {
+        (0x80000000, Context::default()),
+        (0x80000000, Kind::new_auipc(13, 0x0))
+    }
+    payload::AddressInfo {
+        address: 0x20,
+        notify: false,
+        updiscon: false,
+        irdepth: None,
+    } => {
+        (0x80000004, UNCOMPRESSED),
+        (0x80000008, UNCOMPRESSED),
+        (0x8000000c, Kind::new_c_jal(1, 0x14)),
+        (0x80000020, COMPRESSED)
+    }
+    payload::AddressInfo {
+        address: 0x20-0x20,
+        notify: false,
+        updiscon: false,
+        irdepth: None,
+    } => {
+        (0x80000022, Kind::new_c_jr(1)),
+        (0x8000000e, Kind::new_auipc(13, 0)),
+        (0x80000012, Kind::new_jalr(1, 13, 0x12)),
+        (0x80000020, COMPRESSED),
+        (0x80000022, Kind::new_c_jr(1)),
+        (0x80000016, Kind::new_lui(13,0x80000000u32 as i32)),
+        (0x8000001a, Kind::new_jalr(1, 13, 0x20)),
+        (0x80000020, COMPRESSED)
+    }
+    payload::AddressInfo {
+        address: 0x24-0x20,
+        notify: false,
+        updiscon: false,
+        irdepth: None,
+    } => {
+        (0x80000022, Kind::new_c_jr(1)),
+        (0x8000001e, Kind::new_c_j(0, 0x6)),
+        (0x80000024, Kind::wfi)
+    }
+);
+
 fn test_bin_fncalls() -> [(u64, instruction::Instruction); 13] {
     [
         (0x80000000, Kind::new_auipc(13, 0).into()),
