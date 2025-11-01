6124c29375773afffe5c46acb83a6db56441dfe8
Julian Ganz <ganz@fzi.de>
2025-11-01 15:11:06 +0100

    tracer: remove special v1 support logic from Tracer
    Originally, the tracer was essentially transcribed from pseudeo-code
    found in version 1 of the trace specification and later adapted to
    version 2, with the intention of preserving support for version 1.
    Supposed differences in the tracing algorithm were handled via a
    feature.
    
    When we set out to reshape `Tracer` into what it is today, we took care
    to preserve the logic for supporting version 1. However, did not get
    around to actually validate version 1 support until recently. When we
    checked the specification and the reference implementation, there
    appears to be no mismatch between version 1.1.3 and 2.0.3 that would
    warrant the distinction —aside from the pseudeo-code which we did not
    check in detail.
    
    Since the reference implementation did not change, we assume that the
    version 1 specific code does actually not provide functionality the
    version 2 cases do. On the contrary, the version 1 cases are likely to
    cause issues. We therefore decided to remove this logic and make the
    version setting for `Tracer` a dummy setting for the time being.

--- src/tracer.rs
+++ src/tracer.rs
@@ -168,22 +168,15 @@ impl<B: Binary<I>, S: ReturnStack, I: Info + Clone + Default> Tracer<B, S, I> {
         match sync {
             Synchronization::Start(start) => {
                 let is_tracing = self.iter_state.is_tracing();
-                let version = self.version;
 
                 let mut initer = self.sync_init(start.address, !is_tracing, !start.branch)?;
                 if is_tracing && previous != Some(Event::Trap { thaddr: false }) {
-                    let action = match version {
-                        Version::V1 => state::SyncAction::Compare,
-                        _ => state::SyncAction::Update,
-                    };
                     initer.set_condition(state::StopCondition::Sync {
                         context: start.ctx.into(),
-                        action,
+                        action: state::SyncAction::Update,
                     });
                 } else {
-                    if version != Version::V1 {
-                        initer.set_context(start.ctx.into());
-                    }
+                    initer.set_context(start.ctx.into());
                     initer.reset_to_address()?;
                     self.iter_state = IterationState::ContextItem {
                         pc: None,
@@ -209,11 +202,8 @@ impl<B: Binary<I>, S: ReturnStack, I: Info + Clone + Default> Tracer<B, S, I> {
                     initer.set_address(trap.address);
                     initer.reset_to_address()?;
                 } else {
-                    let version = self.version;
                     let mut initer = self.sync_init(trap.address, false, !trap.branch)?;
-                    if version != Version::V1 {
-                        initer.set_context(trap.ctx.into());
-                    }
+                    initer.set_context(trap.ctx.into());
                     initer.reset_to_address()?;
                 }
                 self.iter_state = IterationState::TrapItem {
@@ -226,9 +216,7 @@ impl<B: Binary<I>, S: ReturnStack, I: Info + Clone + Default> Tracer<B, S, I> {
             Synchronization::Context(ctx) => {
                 let mut initer = self.state.initializer(&mut self.binary)?;
                 initer.set_stack_depth(None);
-                if self.version != Version::V1 {
-                    initer.set_context(ctx.into());
-                }
+                initer.set_context(ctx.into());
                 self.iter_state = IterationState::ContextItem {
                     pc: None,
                     context: ctx.into(),
