39aedb99150faff9dd164028e7070f2c98ebd32a
Julian Ganz <ganz@fzi.de>
2025-11-21 22:24:16 +0100

    packet: move tests related to encap to dedicated submodule
    We have quite a few tests in the `packet` module. Enough that we want to
    group some of them into submodules.

--- src/packet/tests.rs
+++ src/packet/tests.rs
@@ -1,5 +1,8 @@
 // Copyright (C) 2025 FZI Forschungszentrum Informatik
 // SPDX-License-Identifier: Apache-2.0
+
+mod encap_tests;
+
 use super::*;
 
 use core::num::{NonZeroU8, NonZeroUsize};
@@ -148,22 +151,6 @@ bitstream_test!(
     iaddress_width_p: 64.try_into().unwrap(),
     iaddress_lsb_p: 1.try_into().unwrap()
 );
-
-#[test]
-fn encap_stop() {
-    let mut decoder = Builder::new().build(b"\x00\x00\x00\x00");
-    for _ in 0..4 {
-        assert_eq!(
-            decoder.decode_encap_packet(),
-            Ok(encap::Packet::NullIdle { flow: 0 }),
-        );
-    }
-    assert_eq!(decoder.bytes_left(), 0, "Not at end of buffer");
-    assert_eq!(
-        decoder.decode_encap_packet(),
-        Err(Error::InsufficientData(NonZeroUsize::MIN)),
-    );
-}
 
 #[test]
 fn implicit_return_test_none() {
@@ -445,62 +432,3 @@ bitstream_test!(
     })),
     &PARAMS_32
 );
-
-#[test]
-/* sync::Context packet; timestamp: 8D; srcId: 31h, header: extend:1, flow: 10,
-length: 01001 -> 1100 | 1001 = C9 9 bytes payload */
-fn decode_encap_packet() {
-    let data = b"\xC9\x31\x8D\x73\x00\x00\x00\x00\x19\x41\x00\x08";
-    // For context: To decode encap packet with timestamp & srcId, harte_index_width and timestamp_width need to be defined otherwise assumed as 0
-    let mut decoder = Builder::new()
-        .with_params(&PARAMS_32)
-        .with_timestamp_width(1)
-        .with_hart_index_width(8)
-        .build(data);
-    let encap_packet: encap::Packet<'_, '_, _> = Decode::decode(&mut decoder).unwrap();
-    assert_eq!(encap_packet.flow(), 2);
-    let normal_encap = encap_packet.into_normal().unwrap();
-    assert_eq!(normal_encap.flow(), 2);
-    assert_eq!(normal_encap.src_id(), 0x31);
-    assert_eq!(normal_encap.timestamp(), Some(0x8D));
-    assert_eq!(
-        normal_encap.payload().unwrap(),
-        payload::Payload::InstructionTrace(InstructionTrace::Synchronization(
-            sync::Synchronization::Start(sync::Start {
-                branch: true,
-                ctx: sync::Context {
-                    privilege: types::Privilege::Machine,
-                    time: None,
-                    context: Some(0)
-                },
-                address: 536937572
-            })
-        )),
-    );
-}
-
-// header reverse: 0010 | 0000, srdId: 0001 | 0000;
-#[test]
-fn decode_encap_null_idle() {
-    let data = b"\x20\x10";
-    let mut decoder = Builder::new()
-        .with_params(&PARAMS_32)
-        .with_hart_index_width(8)
-        .build(data);
-    let encap_data: encap::Packet<'_, '_, _> = Decode::decode(&mut decoder).unwrap();
-    assert_eq!(encap_data.flow(), 1);
-    assert!(encap_data.is_null());
-    assert_eq!(encap_data.into_normal(), None);
-}
-
-#[test]
-// header reverse: 1110 | 0000; timestamp: 0001 | 1001
-fn decode_encap_null_align() {
-    let data = b"\xE0\x10";
-    let mut decoder = Builder::new()
-        .with_params(&PARAMS_32)
-        .with_timestamp_width(1)
-        .build(data);
-    let encap_data: encap::Packet<'_, '_, _> = Decode::decode(&mut decoder).unwrap();
-    assert_eq!(encap_data.flow(), 3);
-}
---
+++ src/packet/tests/encap_tests.rs
@@ -0,0 +1,79 @@
+// Copyright (C) 2025 FZI Forschungszentrum Informatik
+// SPDX-License-Identifier: Apache-2.0
+//! Tets related to RISC-V Encapsulation packets
+use super::*;
+
+#[test]
+fn encap_stop() {
+    let mut decoder = Builder::new().build(b"\x00\x00\x00\x00");
+    for _ in 0..4 {
+        assert_eq!(
+            decoder.decode_encap_packet(),
+            Ok(encap::Packet::NullIdle { flow: 0 }),
+        );
+    }
+    assert_eq!(decoder.bytes_left(), 0, "Not at end of buffer");
+    assert_eq!(
+        decoder.decode_encap_packet(),
+        Err(Error::InsufficientData(NonZeroUsize::MIN)),
+    );
+}
+
+/* sync::Context packet; timestamp: 8D; srcId: 31h, header: extend:1, flow: 10,
+length: 01001 -> 1100 | 1001 = C9 9 bytes payload */
+#[test]
+fn decode_packet() {
+    let data = b"\xC9\x31\x8D\x73\x00\x00\x00\x00\x19\x41\x00\x08";
+    // For context: To decode encap packet with timestamp & srcId, harte_index_width and timestamp_width need to be defined otherwise assumed as 0
+    let mut decoder = Builder::new()
+        .with_params(&PARAMS_32)
+        .with_timestamp_width(1)
+        .with_hart_index_width(8)
+        .build(data);
+    let encap_packet: encap::Packet<'_, '_, _> = Decode::decode(&mut decoder).unwrap();
+    assert_eq!(encap_packet.flow(), 2);
+    let normal_encap = encap_packet.into_normal().unwrap();
+    assert_eq!(normal_encap.flow(), 2);
+    assert_eq!(normal_encap.src_id(), 0x31);
+    assert_eq!(normal_encap.timestamp(), Some(0x8D));
+    assert_eq!(
+        normal_encap.payload().unwrap(),
+        payload::Payload::InstructionTrace(InstructionTrace::Synchronization(
+            sync::Synchronization::Start(sync::Start {
+                branch: true,
+                ctx: sync::Context {
+                    privilege: types::Privilege::Machine,
+                    time: None,
+                    context: Some(0)
+                },
+                address: 536937572
+            })
+        )),
+    );
+}
+
+// header reverse: 0010 | 0000, srdId: 0001 | 0000;
+#[test]
+fn decode_null_idle() {
+    let data = b"\x20\x10";
+    let mut decoder = Builder::new()
+        .with_params(&PARAMS_32)
+        .with_hart_index_width(8)
+        .build(data);
+    let encap_data: encap::Packet<'_, '_, _> = Decode::decode(&mut decoder).unwrap();
+    assert_eq!(encap_data.flow(), 1);
+    assert!(encap_data.is_null());
+    assert_eq!(encap_data.into_normal(), None);
+}
+
+// header reverse: 1110 | 0000; timestamp: 0001 | 1001
+#[test]
+fn decode_null_align() {
+    let data = b"\xE0\x10";
+    let mut decoder = Builder::new()
+        .with_params(&PARAMS_32)
+        .with_timestamp_width(1)
+        .build(data);
+    let encap_data: encap::Packet<'_, '_, _> = Decode::decode(&mut decoder).unwrap();
+    assert_eq!(encap_data.flow(), 3);
+}
