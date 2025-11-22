176d29b06767f000cfc45086e9f1ca75c01de10d
Julian Ganz <ganz@fzi.de>
2025-11-22 11:49:45 +0100

    packet: move tests regarding extraction of parts to dedicated submodule
    We have quite a few tests in the `packet` module. Enough that we want to
    group some of them into submodules.

--- src/packet/tests.rs
+++ src/packet/tests.rs
@@ -3,6 +3,7 @@
 
 mod basic;
 mod encap_tests;
+mod parts;
 
 use super::*;
 
@@ -12,7 +13,6 @@ use crate::types::{self, branch};
 
 use decoder::Decode;
 use payload::{AddressInfo, InstructionTrace};
-use util::read_implicit_return;
 
 macro_rules! bitstream_test {
     ($n:ident, $b:literal, $d:expr) => {
@@ -95,48 +95,6 @@ bitstream_test!(
     iaddress_lsb_p: 1.try_into().unwrap()
 );
 
-#[test]
-fn implicit_return_test_none() {
-    let data = b"\x54\x42\x03\x00\x04\x00\x00\x80";
-
-    let builder = Builder::new();
-    let mut decoder = builder
-        .with_params(&config::Parameters {
-            call_counter_size_p: 1,
-            ..Default::default()
-        })
-        .build(data);
-    decoder
-        .read_bits::<u8>(5)
-        .expect("Tried read bit, but failed");
-    let result = read_implicit_return(&mut decoder);
-    assert_eq!(result, Ok(Some(1)));
-}
-
-#[test]
-fn implicit_return_test_empty() {
-    let data = b"";
-    let builder = Builder::new();
-    let mut decoder = builder.build(data);
-    let result = read_implicit_return(&mut decoder);
-    assert!(matches!(result, Err(Error::InsufficientData(_))));
-}
-
-#[test]
-fn implicit_return_error_depth() {
-    // let data = b"";
-    let data: &[u8] = &[]; // no bytes at all
-    let builder = Builder::new();
-    let mut decoder = builder
-        .with_params(&config::Parameters {
-            call_counter_size_p: 1,
-            ..Default::default()
-        })
-        .build(data);
-    let result = read_implicit_return(&mut decoder);
-    assert!(matches!(result, Err(Error::InsufficientData(_))));
-}
-
 const PARAMS_32: config::Parameters = config::Parameters {
     cache_size_p: 0,
     call_counter_size_p: 0,
@@ -215,99 +173,6 @@ bitstream_test!(
     }),
     &PARAMS_64
 );
-
-macro_rules! branch_taken_test {
-    ($name: ident, $data:expr, $params:expr, $expected:expr) => {
-        #[test]
-        fn $name() {
-            let mut decoder = Builder::new().with_params($params).build($data);
-            let instruction_trace: payload::InstructionTrace =
-                Decode::decode(&mut decoder).unwrap();
-            let sync_trace: sync::Synchronization = match instruction_trace {
-                InstructionTrace::Synchronization(s) => s,
-                _ => panic!("expected synchronization packet"),
-            };
-
-            assert_eq!(sync_trace.branch_not_taken(), $expected);
-        }
-    };
-}
-
-// Format 3 subformat 0 - Synchronisation; 1100; first 2 hex:  0110 | 0011; last 0 in 0110,  indicates branch is taken therefore false
-branch_taken_test!(
-    sync_start_format,
-    b"\x63\x00\x00\x00\x00\x19\x41\x00\x08",
-    &PARAMS_32,
-    Some(false)
-);
-// Format 3 subformat 1 - Trap; 1101; first 2 hex:  0111 | 0111, expected 1011 for reversed format, but encodes none
-branch_taken_test!(
-    sync_trap_format,
-    b"\x77\x00\x00\x00\x00\x19\x41\x00\x08",
-    &PARAMS_32,
-    Some(true)
-);
-// Format 3 subformat 3 - Support; 1111; first 2 hex: 0101 | 1111
-branch_taken_test!(
-    sync_supp_format,
-    b"\x5F\x00\x00\x00\x00\x19\x41\x00\x08",
-    &PARAMS_32,
-    None
-);
-
-macro_rules! as_context_test {
-    ($name: ident, $data:expr, $params:expr, $expected:expr) => {
-        #[test]
-        fn $name() {
-            let mut decoder = Builder::new().with_params($params).build($data);
-            let instruction_trace: payload::InstructionTrace =
-                Decode::decode(&mut decoder).unwrap();
-            let sync_trace: sync::Synchronization = match instruction_trace {
-                InstructionTrace::Synchronization(s) => s,
-                _ => panic!("expected synchronization packet"),
-            };
-
-            assert_eq!(sync_trace.as_context(), $expected);
-        }
-    };
-}
-
-as_context_test!(
-    sync_start_packet,
-    b"\x63\x00\x00\x00\x00\x19\x41\x00\x08",
-    &PARAMS_32,
-    Some(&sync::Context {
-        privilege: types::Privilege::Machine,
-        time: None,
-        context: Some(0)
-    })
-);
-
-// fmt: 11, subfmt: 10, priv: 11, time: 0, ctx: 0 -> 0011 | 1011 = 3Bh
-as_context_test!(
-    sync_ctx_packet,
-    b"\x3B\x00",
-    &PARAMS_32,
-    Some(&sync::Context {
-        privilege: types::Privilege::Machine,
-        time: None,
-        context: Some(0)
-    })
-);
-
-// Fmt: 11, subfmt: 01, branch: 1, priv: 11, time: (non-existent), ctx: 1 -> 1111 | 0111 = F7h
-as_context_test!(
-    sync_trap_packet,
-    b"\xF7\x00\x00\x00\x00\x19\x41\x00\x08",
-    &PARAMS_32,
-    Some(&sync::Context {
-        privilege: types::Privilege::Machine,
-        time: None,
-        context: Some(1)
-    })
-);
-// Fmt: 11, subfmt: 11, ienable: 1, !enc_mode: 0, qual_stat: 00
-as_context_test!(sync_supp_packet, b"\x1F\x00", &PARAMS_32, None);
 
 // Ok(Synchronization(Support(Support { ienable: true, encoder_mode: BranchTrace, qual_status: TraceLost, ioptions: ReferenceIOptions { implicit_return: false, implicit_exception: false, full_address: false, jump_target_cache: false, branch_prediction: false }, denable: false, dloss: false, doptions: ReferenceDOptions { no_address: false, no_data: false, full_address: false, full_data: false } })))
 bitstream_test!(
---
+++ src/packet/tests/parts.rs
@@ -0,0 +1,141 @@
+// Copyright (C) 2025 FZI Forschungszentrum Informatik
+// SPDX-License-Identifier: Apache-2.0
+//! Tests focussing on extraction of specialized parts in particular
+use super::*;
+
+use util::read_implicit_return;
+
+#[test]
+fn implicit_return_test_none() {
+    let data = b"\x54\x42\x03\x00\x04\x00\x00\x80";
+
+    let builder = Builder::new();
+    let mut decoder = builder
+        .with_params(&config::Parameters {
+            call_counter_size_p: 1,
+            ..Default::default()
+        })
+        .build(data);
+    decoder
+        .read_bits::<u8>(5)
+        .expect("Tried read bit, but failed");
+    let result = read_implicit_return(&mut decoder);
+    assert_eq!(result, Ok(Some(1)));
+}
+
+#[test]
+fn implicit_return_test_empty() {
+    let data = b"";
+    let builder = Builder::new();
+    let mut decoder = builder.build(data);
+    let result = read_implicit_return(&mut decoder);
+    assert!(matches!(result, Err(Error::InsufficientData(_))));
+}
+
+#[test]
+fn implicit_return_error_depth() {
+    // let data = b"";
+    let data: &[u8] = &[]; // no bytes at all
+    let builder = Builder::new();
+    let mut decoder = builder
+        .with_params(&config::Parameters {
+            call_counter_size_p: 1,
+            ..Default::default()
+        })
+        .build(data);
+    let result = read_implicit_return(&mut decoder);
+    assert!(matches!(result, Err(Error::InsufficientData(_))));
+}
+
+macro_rules! branch_taken_test {
+    ($name: ident, $data:expr, $params:expr, $expected:expr) => {
+        #[test]
+        fn $name() {
+            let mut decoder = Builder::new().with_params($params).build($data);
+            let instruction_trace: payload::InstructionTrace =
+                Decode::decode(&mut decoder).unwrap();
+            let sync_trace: sync::Synchronization = match instruction_trace {
+                InstructionTrace::Synchronization(s) => s,
+                _ => panic!("expected synchronization packet"),
+            };
+
+            assert_eq!(sync_trace.branch_not_taken(), $expected);
+        }
+    };
+}
+
+// Format 3 subformat 0 - Synchronisation; 1100; first 2 hex:  0110 | 0011; last 0 in 0110,  indicates branch is taken therefore false
+branch_taken_test!(
+    sync_start_format,
+    b"\x63\x00\x00\x00\x00\x19\x41\x00\x08",
+    &PARAMS_32,
+    Some(false)
+);
+// Format 3 subformat 1 - Trap; 1101; first 2 hex:  0111 | 0111, expected 1011 for reversed format, but encodes none
+branch_taken_test!(
+    sync_trap_format,
+    b"\x77\x00\x00\x00\x00\x19\x41\x00\x08",
+    &PARAMS_32,
+    Some(true)
+);
+// Format 3 subformat 3 - Support; 1111; first 2 hex: 0101 | 1111
+branch_taken_test!(
+    sync_supp_format,
+    b"\x5F\x00\x00\x00\x00\x19\x41\x00\x08",
+    &PARAMS_32,
+    None
+);
+
+macro_rules! as_context_test {
+    ($name: ident, $data:expr, $params:expr, $expected:expr) => {
+        #[test]
+        fn $name() {
+            let mut decoder = Builder::new().with_params($params).build($data);
+            let instruction_trace: payload::InstructionTrace =
+                Decode::decode(&mut decoder).unwrap();
+            let sync_trace: sync::Synchronization = match instruction_trace {
+                InstructionTrace::Synchronization(s) => s,
+                _ => panic!("expected synchronization packet"),
+            };
+
+            assert_eq!(sync_trace.as_context(), $expected);
+        }
+    };
+}
+
+as_context_test!(
+    sync_start_packet,
+    b"\x63\x00\x00\x00\x00\x19\x41\x00\x08",
+    &PARAMS_32,
+    Some(&sync::Context {
+        privilege: types::Privilege::Machine,
+        time: None,
+        context: Some(0)
+    })
+);
+
+// fmt: 11, subfmt: 10, priv: 11, time: 0, ctx: 0 -> 0011 | 1011 = 3Bh
+as_context_test!(
+    sync_ctx_packet,
+    b"\x3B\x00",
+    &PARAMS_32,
+    Some(&sync::Context {
+        privilege: types::Privilege::Machine,
+        time: None,
+        context: Some(0)
+    })
+);
+
+// Fmt: 11, subfmt: 01, branch: 1, priv: 11, time: (non-existent), ctx: 1 -> 1111 | 0111 = F7h
+as_context_test!(
+    sync_trap_packet,
+    b"\xF7\x00\x00\x00\x00\x19\x41\x00\x08",
+    &PARAMS_32,
+    Some(&sync::Context {
+        privilege: types::Privilege::Machine,
+        time: None,
+        context: Some(1)
+    })
+);
+// Fmt: 11, subfmt: 11, ienable: 1, !enc_mode: 0, qual_stat: 00
+as_context_test!(sync_supp_packet, b"\x1F\x00", &PARAMS_32, None);
