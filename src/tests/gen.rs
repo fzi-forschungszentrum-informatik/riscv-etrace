3a3db14963b1e587c9c6b35953b2d329b27e37f3
Julian Ganz <ganz@fzi.de>
2025-12-10 10:27:08 +0100

    tests: make trace_test! accept hints in items
    We plan to introduce trace encoding functionality in the near future.
    For testing that functionality, we intend to use existing test-cases.
    However, some aspects such as (non)taken branches before an exception
    are expressed via packets but not visible in the items from which we
    will derive the generator input. We therefore need to introduce
    additional hints.
    
    This change allows placing such hints without breaking the existing
    generation of `decoder` and `size_hint` tests.

--- src/tests/gen.rs
+++ src/tests/gen.rs
@@ -72,7 +72,7 @@ macro_rules! trace_test_helper {
 }
 
 macro_rules! trace_check_def {
-    ($t:ident, ($a:literal, $i:expr)) => {
+    ($t:ident, ($a:literal, $i:expr $(, $h:ident)*)) => {
         assert_eq!($t.next(), Some(Ok(Item::new($a, $i.into()))));
     };
     ($t:ident, [$($i:tt),*; $n:literal]) => {
@@ -88,7 +88,7 @@ macro_rules! trace_check_def {
 }
 
 macro_rules! trace_item_count {
-    (($a:literal, $i:expr)) => { 1 };
+    (($a:literal, $i:expr $(, $h:ident)*)) => { 1 };
     ([$($i:tt),*; $n:literal]) => { $n * trace_item_count!($($i),*) };
     ($($i:tt),*) => { 0usize $( + trace_item_count!($i) )* };
 }
