ec42c787104ec9bb31c668d07e4ff4c58195a088
Ken <uhjmj@student.kit.edu>
2026-01-17 13:58:34 +0100

    types: reworked BoxStack for stack implementation
    Reworked design of the BoxStack implementation. No longer an impl of
    ReturnStack, since features as depth and max_depth were not fit for a
    Box. Unit tests also made fit to work for new implementation of the
    BoxStack.

--- src/types/tests.rs
+++ src/types/tests.rs
@@ -43,9 +43,28 @@ underflow_test!(static_stack_under, StaticStack<2>);
 return_stack_implementation!(static_implementation, StaticStack<3>);
 
 #[cfg(feature = "alloc")]
-mod alloc_stack {
+mod box_stack_test {
     use super::*;
     use crate::types::stack::BoxStack;
+    use crate::types::stack::ReturnStackBox;
+    #[test]
+    fn test_box_return_stack() {
+        let mut box_stack = ReturnStackBox::new().unwrap();
+        assert!(box_stack.pop().is_none(), "Expected box to be empty");
+        box_stack.push(10);
+        box_stack.push(555);
+        box_stack.push(10000);
+
+        assert_eq!(box_stack.pop(), Some(10000));
+        assert_eq!(box_stack.pop(), Some(555));
+
+        box_stack.push(35);
+        assert_eq!(box_stack.pop(), Some(35));
+        assert_eq!(box_stack.pop(), Some(10));
+        assert_eq!(box_stack.pop(), None);
+    }
+}
+
     use crate::types::stack::VecStack;
 
     underflow_test!(vector_stack_under, VecStack);
