b86e1581c642c4a46fede8022c0a60cea2ed7ba2
Julian Ganz <ganz@fzi.de>
2026-01-23 18:52:02 +0100

    packet: derive Default for sync::QualStatus
    ... rather than implementing it by hand.

--- src/packet/sync.rs
+++ src/packet/sync.rs
@@ -332,9 +332,10 @@ where
 }
 
 /// Representation of a change to the filter qualification
-#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+#[derive(Copy, Clone, Default, Debug, Eq, PartialEq)]
 pub enum QualStatus {
     /// No change to filter qualification.
+    #[default]
     NoChange,
     /// Qualification ended, preceding packet sent explicitly to indicate last
     /// qualification instruction.
@@ -344,12 +345,6 @@ pub enum QualStatus {
     /// Qualification ended, preceding packet would have been sent anyway due to
     /// an updiscon, even if it wasn’t the last qualified instruction
     EndedNtr,
-}
-
-impl Default for QualStatus {
-    fn default() -> Self {
-        Self::NoChange
-    }
 }
 
 impl<U> Decode<'_, '_, U> for QualStatus {
