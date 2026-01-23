192a89c72ccf9b2ae1eedcd78d3720334a7ff7a8
Julian Ganz <ganz@fzi.de>
2026-01-23 12:19:31 +0100

    generator: rename Drain::gen data member to generator
    `gen` is scheduled to become a keyword in some future version of Rust.
    We currently ger around this because it only became a reserved word in
    Rust edition 2024 and we're using edition 2021. However, we intend to
    upgrade to the new edition and thus need to eliminate any use of `gen`.

--- src/generator.rs
+++ src/generator.rs
@@ -367,16 +367,16 @@ impl Builder {
 /// Payload draining [`Iterator`]
 #[derive(Debug)]
 pub struct Drain<'g, S: step::Step, I: unit::IOptions, D> {
-    gen: &'g mut Generator<S, I, D>,
+    generator: &'g mut Generator<S, I, D>,
     ienable: bool,
     qual_status: Option<sync::QualStatus>,
 }
 
 impl<'g, S: step::Step, I: unit::IOptions, D> Drain<'g, S, I, D> {
     /// Create a new draining [`Iterator`]
-    fn new(gen: &'g mut Generator<S, I, D>, ienable: bool) -> Self {
+    fn new(generator: &'g mut Generator<S, I, D>, ienable: bool) -> Self {
         Drain {
-            gen,
+            generator,
             ienable,
             qual_status: Some(sync::QualStatus::EndedRep),
         }
@@ -387,13 +387,13 @@ impl<S: step::Step + Clone, I: unit::IOptions + Clone, D: Clone> Iterator for Dr
     type Item = Result<InstructionTrace<I, D>, Error>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        match self.gen.do_step(None, None) {
+        match self.generator.do_step(None, None) {
             Ok(Some(StepOutput::Payload(p))) => {
                 self.qual_status = Some(sync::QualStatus::EndedNtr);
                 Some(Ok(p))
             }
             Ok(Some(StepOutput::Builder(b))) => Some(b.report_address(state::Reason::Other)),
-            Ok(None) => Option::zip(self.gen.options.clone(), self.qual_status.take()).map(
+            Ok(None) => Option::zip(self.generator.options.clone(), self.qual_status.take()).map(
                 |((ioptions, doptions), qual_status)| {
                     Ok(sync::Support {
                         ienable: self.ienable,
